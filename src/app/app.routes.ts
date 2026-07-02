36acc3efbb6fc69f8401a62cbe084207d198f8dd
hanna-adel <adelhana895@gmail.com>
2026-07-03 00:40:52 +0300

    Wire up landing page and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LandingPage } from './Features/landing-page/landing-page';
+export const routes: Routes = [
+    {path: '',component: LandingPage}
+];
