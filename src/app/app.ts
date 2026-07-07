70f524d609a4a085a94589e699438d4256a3f675
Ziad Zaki <ziad.mahmoud151@gmail.com>
2026-07-07 18:25:57 +0300

    added purchaseOrder Get

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { PublicLayout } from './Features/Layout/Public Layout/Components/public-layout';
 import { privatelayout } from './Features/Layout/Private Layout/Components/private-layout';
 import { PharmacySettings } from "./Features/settings/pharmacy-settings/pharmacy-settings";
+import { PurchaseOrderPage } from "./Features/PurchaseOrder/purchase-order-page/purchase-order-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PublicLayout, privatelayout, PharmacySettings],
+  imports: [RouterOutlet, PublicLayout, privatelayout, PharmacySettings, PurchaseOrderPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
