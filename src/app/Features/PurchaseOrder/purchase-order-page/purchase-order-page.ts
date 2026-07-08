0f23112bd65af1b9dced0ad4630b00233796300f
Ziad Zaki <ziad.mahmoud151@gmail.com>
2026-07-08 18:40:10 +0300

    added purchase-order create method

--- src/app/Features/PurchaseOrder/purchase-order-page/purchase-order-page.ts
+++ src/app/Features/PurchaseOrder/purchase-order-page/purchase-order-page.ts
@@ -2,16 +2,37 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { PurchaseOrderApiService } from '../Services/purchase-order-api';
 import { ChangeDetectorRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-purchase-order-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './purchase-order-page.html',
   styleUrl: './purchase-order-page.css',
 })
 export class PurchaseOrderPage implements OnInit {
   purchaseOrders: any[] = [];
+
+  showCreateModal = false;
+
+  suppliers: any[] = [];
+
+  Medicines: any[] = [];
+  selectedMedicine: string = '';
+
+  purchaseOrder = {
+    orderDate: '',
+    expectedDate: '',
+    supplierId: '',
+    items: [
+      {
+        medicineId: '',
+        quantityOrdered: 1,
+        unitPrice: 0,
+      },
+    ],
+  };
 
   constructor(
     private purchaseOrderService: PurchaseOrderApiService,
@@ -34,6 +55,31 @@ export class PurchaseOrderPage implements OnInit {
     });
   }
 
+  loadSuppliers() {
+    this.purchaseOrderService.getSuppliers().subscribe({
+      next: (res: any) => {
+        this.suppliers = res;
+        console.log(res);
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+  loadMedicines() {
+    this.purchaseOrderService.getMedicines().subscribe({
+      next: (res: any) => {
+        this.Medicines = res;
+        console.log(res);
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
   get openOrders() {
     return this.purchaseOrders.filter((x) => x.status === 'Open').length;
   }
@@ -48,6 +94,56 @@ export class PurchaseOrderPage implements OnInit {
 
   get totalLines() {
     return this.purchaseOrders.reduce((sum, x) => sum + x.lines, 0);
+  }
+  get totalPrice() {
+    var total = 0;
+    this.purchaseOrder.items.map((x) => (total += x.unitPrice * x.quantityOrdered));
+    return total;
+  }
+
+  openCreateModal() {
+    this.showCreateModal = true;
+    this.loadSuppliers();
+    this.loadMedicines();
+  }
+
+  closeCreateModal() {
+    this.showCreateModal = false;
+  }
+
+  addLine() {
+    this.purchaseOrder.items.push({
+      medicineId: '',
+      quantityOrdered: 1,
+      unitPrice: 0,
+    });
+  }
+  removeLine(index: number) {
+    this.purchaseOrder.items.splice(index, 1);
+  }
+
+  onMedicineChange(line: any) {
+    const medicine = this.Medicines.find((m) => m.id === line.medicineId);
+
+    if (medicine) {
+      line.unitPrice = medicine.purchasePrice;
+    }
+  }
+
+  createPurchaseOrder() {
+    console.log(this.purchaseOrder);
+
+    this.purchaseOrderService.addPurchaseOrder(this.purchaseOrder).subscribe({
+      next: (res) => {
+        console.log(res);
+
+        this.closeCreateModal();
+        this.loadPurchaseOrders();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
   statusClass(status: string) {
